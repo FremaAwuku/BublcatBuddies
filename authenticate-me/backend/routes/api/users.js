1dc7c8e0e692fcf723fccecc79c43177d6a9dbad
Frema Awuku <frema.awuku@gmail.com>
2021-08-12 15:32:50 -0400

    phase 1 complete

--- authenticate-me/backend/routes/api/users.js
+++ authenticate-me/backend/routes/api/users.js
@@ -1,29 +1,56 @@
 const express = require('express')
 const router = express.Router();
 const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+//uses express validator to check user inputs
+//passes the handleValidationErrors
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors,
+];
+
+
+
 /** router.post('/')
+ * verify users inputs through validateSignup
  * handles user sign up by extracting user input from req body
  * uses User.signup(User Model static method) to sign up user
  * sets cookie Token
  * returns json of user
  */
-router.post(
-    '/',
-    asyncHandler(async (req, res) => {
-      const { email, password, username } = req.body;
-      const user = await User.signup({ email, username, password });
+ router.post(
+  '/',
+  validateSignup,
+  asyncHandler(async (req, res) => {
+    const { email, password, username } = req.body;
+    const user = await User.signup({ email, username, password });
 
-      await setTokenCookie(res, user);
+    await setTokenCookie(res, user);
 
-      return res.json({
-        user,
-      });
-    }),
-  );
-
+    return res.json({
+      user,
+    });
+  }),
+);
 
 
 
