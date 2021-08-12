100885987e5dabd96e10d6245da71a2ab8e10d37
Frema Awuku <frema.awuku@gmail.com>
2021-08-12 11:33:40 -0400

    last minut commits

--- authenticate-me/backend/db/models/user.js
+++ authenticate-me/backend/db/models/user.js
@@ -56,7 +56,30 @@ User.prototype.validatePassword = function (password) {
  User.getCurrentUserById = async function (id) {
   return await User.scope('currentUser').findByPk(id);
  };
- 
+ User.login = async function ({ credential, password }) {
+  const { Op } = require('sequelize');
+  const user = await User.scope('loginUser').findOne({
+    where: {
+      [Op.or]: {
+        username: credential,
+        email: credential,
+      },
+    },
+  });
+  if (user && user.validatePassword(password)) {
+    return await User.scope('currentUser').findByPk(user.id);
+  }
+};
+User.signup = async function ({ username, email, password }) {
+  const hashedPassword = bcrypt.hashSync(password);
+  const user = await User.create({
+    username,
+    email,
+    hashedPassword,
+  });
+  return await User.scope('currentUser').findByPk(user.id);
+};
+
 User.associate = function(models) {
     // associations can be defined here
   };
