43892df4b87a95d3cda2b38b98bb2762cff11bb7
Frema Awuku <frema.awuku@gmail.com>
2021-08-12 21:29:06 -0400

    session store setup/login/logout/signup forms created

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,8 +4,7 @@ const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { restoreUser } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
-
-
+const { requireAuth } = require('../../utils/auth.js');
 
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
@@ -26,39 +25,40 @@ router.use('/users', usersRouter);
 //testing setTokenCookie function from utils/auth.js
 //>>>> returns value of setTokenCookie's scope with correct insensitive information
 //>>>>{id & username}
-// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
-//   const user = await User.findOne({
-//       where: {
-//         username: 'Demo-lition'
-//       },
-//     })
-//   setTokenCookie(res, user);
-//   return res.json({ user });
-// }));
+router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      },
+    })
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
 
 //testing restoreUser function from utils/auth.js
   //>>>> returns value of restoreUser's scope with correct insensitive information
   //>>>{id,username,email,created/updated}
-// router.get(
-//   '/restore-user',
-//   restoreUser,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
+router.get(
+  '/restore-user',
+  restoreUser,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 
 //testing requireAuth function from utils/auth.js
 //returns value of restoreUser's scope {id,username,email,created/updated}
 //ONLY IF COOKIE TOKEN has previously been set
 //returns "Unauthorized" Error if cookie token has not been set
-// const { requireAuth } = require('../../utils/auth.js');
-// router.get(
-//   '/require-auth',
-//   requireAuth,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
+
+
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 
 
 module.exports = router;
